198a1a3e3839165cb01cab87a3606c4e47ce657e
Ryan <Ryan@LAPTOP-V66DSNAN>
2017-02-14 13:49:49 -0500

    Set up some basic instance variables for BB_Book and BB_Page classes.

--- BookBuilder/Program.cs
+++ BookBuilder/Program.cs
@@ -6,6 +6,39 @@ using System.Threading.Tasks;
 
 namespace BookBuilder
 {
+
+    class BB_Page
+    {
+        public int pageNumber;
+
+        public string pageImageName;
+        public string videoFileName;
+        public int videoWidth;
+        public int videoHeight;
+        public int videoX;//x coord of video
+        public int videoY;//y coord of video
+        //public ??? videoCRC; //the CRC hash for the video will go here
+        //public ??? pageCRC //the CRC hash for the page image will go here
+
+    }
+
+    //Book for the bookbuilder
+    //Prefix so there's no confusion with the ARMB Book class.
+    class BB_Book
+    {
+        public List<BB_Page> pages;
+        public String title;
+        public List<String> authors;
+        public String creationDate;
+        public String description;
+
+        //filename of the button_image
+        public String buttonImageName;
+        public String fileVersion;
+
+
+    }
+
     class Program
     {
         static void Main(string[] args)
